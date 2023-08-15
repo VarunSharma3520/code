35044294833e27a9bb2fd76f6c77f6bf3318bebd
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-15 20:50:00 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -14,5 +14,12 @@ df.copy(deep = False) # deep copy if default
 
 df.size #returns rows * column
 df.index #returns list of rows in an object
-df.shape # returns (column , rows)
-
+df.shape # returns (rows , column)
+df.memory_usage() #returns memory in bytes
+df.ndim #returns arr dimension
+df.head() #returns first 5 rows from dataframe
+df.head(10) #returns first n rows from dataframe
+df.tail() #returns last 5 rows from dataframe
+df.tail(10) #returns last n rows from dataframe
+df.at[4, 'total_rooms'] #seeks a value at df.at(index_value,column_name)
+df.at[4, 'total_rooms'] = 10 # sets a value at df
