2b85b8efecdd0166caeb4fd6a92e8f2a086fd2cf
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-15 21:58:11 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('sample_data/california_housing_test.csv')
+# df = pd.read_csv('sample_data/california_housing_test.csv',na_values=['??','????'])
 
 # print(type(df)) #<class 'pandas.core.frame.DataFrame'>
 print(df.to_string()) 
@@ -23,3 +24,18 @@ df.tail() #returns last 5 rows from dataframe
 df.tail(10) #returns last n rows from dataframe
 df.at[4, 'total_rooms'] #seeks a value at df.at(index_value,column_name)
 df.at[4, 'total_rooms'] = 10 # sets a value at df
+df.iat[4,3] #seeks a value at df.at(row_value,column_value)
+df.iat[4,3] = 11 #sets a value at df.at(row_value,column_value)
+df.loc[:,'total_rooms'] # gets a single table
+
+data Types in pandas 
+character: category, object
+numeric : int64, float64
+
+df.dtypes # returns data types
+df.dtypes.value_counts() # counts type of data types
+df.select_dtypes(include=['float64']) # returns subset of selected datatypes.
+df.info() # gives infomation about memory,total cloumn, rangeIndex, Dtype,non null datatype
+np.unique(df['total_rooms']) # return unique dataTypes
+
+#astypes is used convert datatype in python
