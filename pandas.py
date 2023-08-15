57bc589408484393d96fc42c693db8fa9e1ac942
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-15 20:36:46 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,1 +1,18 @@
+import os
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+# print(type(df)) #<class 'pandas.core.frame.DataFrame'>
+print(df.to_string()) 
+
+# How to copy CSV data in Python 
+# Shallow copy 
+# Deep Copy
+
+df.copy(deep = False) # deep copy if default
+
+df.size #returns rows * column
+df.index #returns list of rows in an object
+df.shape # returns (column , rows)
 
