5f5504070804453e66c88fabde74cdbf483abc70
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-25 19:29:26 +0530

    oue.py

---
+++ Python/Que.py
@@ -0,0 +1,86 @@
+# timing :09:00
+'''
+s=['M','F','F','M','F','M','F']
+m=[91,95,87,85,90,56,100]
+logic: create 
+o/p: [95,100]
+'''
+# s=['M','F','F','M','F','M','F']
+# m=[91,95,87,85,90,56,100]
+# listy=[]
+# for i in range(len(s)):
+#     if s[i]=='F' and m[i]>90:
+#         listy.append(m[i])
+
+# d = {
+#     5:25,
+#     4:16,
+#     5:52,
+#     3:10,
+#     2:4,
+#     10:100
+# }
+# listy = []
+# keys = d.keys()
+# for i in range(len(keys)):
+#     if d[keys[i]] == keys[i]**2:
+#         listy.append(keys[i])
+# print(listy)
+
+# d = {}
+# user_input = int(input('Enter any number : '))
+# for i in range(1,11):
+#     key = str(user_input) + ' X ' + str(i)
+#     value = i*user_input
+#     d.update({key:value})
+# print(d)
+
+
+
+# prices =['$50','$123','@50','$130','@125','$100']
+# d={}
+# for i in range(len(prices)):
+#     if prices[i][0] == '$' and prices[i][-1]== '0' :
+#         key = int(prices[i][1:])//10
+#         value = 10
+#         d.update({key:value})
+# print(d)
+
+# listy = [9354,99101,72891,6268,5784]
+# listji = []
+# for i in range(len(listy)):
+#     if len(str(listy[i])) == 4:
+#         s = str(listy[i])
+#         listji.append(int(s[::-1]))
+# print(listji)
+
+
+# listy = [9354,99101,72891,6268,5784]
+# oplist = []
+# for i in range(len(listy)):
+#     checker = listy[i]//100
+#     if checker % 2 != 0:
+#         oplist.append(listy[i])
+# print(oplist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
