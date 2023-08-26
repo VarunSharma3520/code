19ec1dea413372211f2178ff16202e269b55938c
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-26 18:40:10 +0530

    assignment 1

---
+++ Python/Assignment 1.py
@@ -0,0 +1,53 @@
+# 1. WAP to take 10 numbers from a user and calculate their mean...
+sum = 0
+for i in range(10):
+    user_input = int(input("Enter any number..."))
+    sum = sum + user_input
+print("mean of above number is : ",sum/10)
+
+
+# 2. Display numbers divisible by 5 from a list.
+
+#3. WAP to calculate the number of elements in a string, taken as input from the user, without using len function.
+user_input = input("Enter any number...")
+count = 0
+for i in user_input:
+    count +=1
+print("Length of string is : ",count)
+
+
+#4. WAP to check if a number is even or odd where number is taken as input.
+user_input = int(input("Enter any number..."))
+if user_input % 2 == 0:
+    print(user_input,"is a even number")
+else:
+    print(user_input,"is a odd number")
+
+
+#5. Write a Python program to find the sum of the first n positive integers. Take n as input from the user.
+
+#6. WAP to print all the strings present in a list in reverse fashion.
+
+#7. Print characters from a string that are present at an even index number
+
+#8. Check if the first and last number of a list is the same
+
+#9. Calculate income tax for the input income by adhering to the Indian rules.
+
+#10. Write a Python program to check whether a string starts with vowel or not.
+
+#11. Write a Python program to check if all the characters of a string are vowel or not.
+
+#12. WAP to calculate percentage of a student through 5 subjects. Take marks as input from the user.
+
+#13. WAP to convert '95.3' taken as input from the user into float value.
+
+#14. Write a Python program to count the number of even and odd numbers from a list of numbers. e.g. i\p: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#15. WAP to calculate the surface area of a triangle. Take the required values as input.
+
+
+
+
+
+
