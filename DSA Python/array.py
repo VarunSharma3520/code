22e01df2a73601af284ea457e4da4a179e008234
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-27 14:29:33 +0530

    array in [ython

---
+++ DSA Python/array.py
@@ -0,0 +1,30 @@
+# array v/s list
+# array are more faster than list
+# so we will use numpy array to implement this
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr)
+print(arr[0])
+print(arr[1:5])
+print(type(arr))
+print(arr.dtype)       # checking data types of element s of array
+
+newarr = arr.astype('f')
+print(newarr)
+print(newarr.dtype)
+
+x = arr.copy()
+y = arr.view()
+
+print(x.base)
+print(y.base)
+
+print(arr.shape)        # returns a tuple of (rows, column)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr3 = np.concatenate((arr1, arr2))
+print(arr3)
