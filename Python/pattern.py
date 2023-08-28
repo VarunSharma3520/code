0cf990142897249f8d0d9e8e34460f8d59cbd941
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-28 19:43:35 +0530

    question 3

--- Python/pattern.py
+++ Python/pattern.py
@@ -6,4 +6,29 @@ for i in range(n):
 
 # Question 2
 for i in range(n):
-    
+    print(" "*(i-1),"*"*(n-i))
+
+# Question 3
+for i in range(n+2):
+    print()
+    for j in range(1,n+2):
+        if i > j :
+            print(j,end='')
+
+# Question 5 
+for i in range(n):
+    print(" "*(n-i),"*"*i)
+
+# Question 6
+
+# Question 7
+
+# Question 8
+
+# Question 9
+
+# Question 10
+
+
+
+
