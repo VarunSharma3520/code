4223cdad9b9f7d2bb8642e86fff7c25a69605253
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-08-30 14:04:55 +0530

    que 1 dsa assigmnet  solved

--- DSA Python/assignment.py
+++ DSA Python/assignment.py
@@ -1,8 +1,15 @@
+# Q1: Find number of days above average temperature...
 days = int(input("How many day's temperature ? "))
 temp = []
 sum = 0
 for i in range(days):
-    user_temp = int(input("Day(s)",i,"'s high temperature: "))
+    user_temp = int(input("Day(s) "+str(i+1)+"'s high temperature: "))
     temp.append(user_temp)
     sum += user_temp
-print("Average: ",sum/days)
+avg = sum/days
+sum = 0
+print("Average = ",avg)
+for i in range(len(temp)):
+    if temp[i] > avg:
+        sum = sum + 1
+print(sum,"day(s) above average")
