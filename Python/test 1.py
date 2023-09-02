089a9a723fc6000f29d20d29c4092c4f0b574163
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-09-02 19:10:40 +0530

    commit

--- Python/test 1.py
+++ Python/test 1.py
@@ -20,6 +20,24 @@ if user_input > 2:
 arr1 = [7,8,10,13,90]
 arr2 = [9,11,17,62,81,92]
 def merge_two_sorted_array(arr1,arr2):
+    checker = 0
+    listy = []
     pass
 print(merge_two_sorted_array)
+
+# Que3. Remove an element from an array
+arr1 = [7,8,10,13,90]
+def del_element(arr1,elem):
+    listy = []
+    for i in range(len(arr1)):
+        if arr1[i] != elem:
+            listy.append(arr1[i])
+    return listy
+print(del_element(arr1,13))
+
+
+
+
+
+
 
