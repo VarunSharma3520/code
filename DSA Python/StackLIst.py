1b8dec7719e6e4aa5dab6d618a0bea77668fc86d
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-09-07 02:02:37 +0530

    stackList

---
+++ DSA Python/StackLIst.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.list = []
+        self.length = 0
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
+    def push(self,value):
+        self.list.append(value)
+        self.length += 1
+    def pop(self):
+        self.list.pop()
+        self.length -= 1
+    def peek(self):
+        print("hello")
+        return self.list[len(self.list) - 1]
+    def isEmpty(self):
+        if self.length == 0:
+            return True
+        else:
+            return False
+    def dump():
+        self.list = None
+customStack = Stack()
+customStack.push(1)
+customStack.push(15)
+customStack.push(2)
+customStack.push(0)
+customStack.push(9)
+customStack.pop()
+print(customStack.peek())
+print(customStack.isEmpty())
+print(customStack)
+customStack.dump()
+print(customStack)
