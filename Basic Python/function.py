3deeadad2c3ba7d87881faddb9d132ed7ea9e954
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-09-08 18:47:28 +0530

    c

--- Basic Python/function.py
+++ Basic Python/function.py
@@ -3,5 +3,13 @@ types of argument:
 Required Argument
 keyword Arguement
 Default Argument
-Variable Length Arguement
+Variable Length Arguement 
+"""
+#
+"""
+Variable Length Argument: Eg-> print()
+def add(*a):
+    print('a',a)
+add(5)
+add(5,6,"l",10)
 """
