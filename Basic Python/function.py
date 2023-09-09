1d9c048fb4ca7f539b594cecb2476d48d03fe807
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-09-09 19:57:47 +0530

    map function

--- Basic Python/function.py
+++ Basic Python/function.py
@@ -16,6 +16,36 @@ add(5,6,"l",10)
 
 """
 Lamda Function:
+"""
+
 z = lambda x: x**2
 print(z(2))
+
+
+sql = lambda k:[i**2 for i in k]
+def add():
+    l = [1,2,3,4,5,6,7,8,9]
+    return sql(l)
+print(add())
+
+l = [1,2,3,4,5,6,7,8,9]
+print((lambda k : [i**2 for i in k])(l))
+
+swap = lambda x,y: [y,x]
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,0]
+l1[0],l1[1] = swap(l1[0],l1[1])
+print(l1)
+
 """
+map(funcName,collection)
+"""
+l = [1,2,3,4,5,6,7,8,9]
+n = list(map(str,l))
+print(n)
+
+l1 = '123456'
+l2 = list(l1)
+print([int(x)**2 for x in l2 ])
+l3 = list(map(int,l2))
+print([x**2 for x in l3 ])
