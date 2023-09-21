e91d4a095ed89eca02569df82c197b1bf1cb2eef
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-09-21 19:27:25 +0530

    Exception handling in python

---
+++ Basic Python/Exception Handing.py
@@ -0,0 +1,43 @@
+# Exception Handling
+# Exception is which can interrupt program functioning while excecuting script
+'''
+try:
+except:
+else:
+finally:
+'''
+'''
+In case of error:
+try:
+except:
+finally:
+In case of error not occured:
+try:
+else:
+finally:
+'''
+# def script():
+#     marks = int(input("Enter any number..."))
+#     if marks > 40 :
+#         print("pass")
+#     else:
+#         print("fail")
+# try:
+#     script()
+# except:
+#     print("An Error Occured ")
+#     script()
+
+# def script():
+#     marks = int(input("Enter any number..."))
+#     return marks
+# try:
+#     marks = script()
+# except:
+#     print("An Error Occured ")
+#     marks = script()
+# else:
+#     if marks > 40 :
+#         print("pass")
+#     else:
+#         print("fail")
