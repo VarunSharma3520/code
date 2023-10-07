4703305dbf9947509631d717ca49995c17ba997f
VarunSharma3250 <sv279508@gmail.com>
2023-10-07 19:40:14 +0530

    c

--- Basic Python/classVarInstVar.py
+++ Basic Python/classVarInstVar.py
@@ -1,9 +1,13 @@
+"""
+class Variable and Instance Variable
+"""
 class human():
     count = 0
-    def __init__(self,human,gender):
+    def __init__(self,human,gender): # constructor
         human.count +=1
         self.gender = gender
         self.name = name
+    def __del__(self):  # destructor
 
 h1 = human("Varun","M")
 h2 = human("Nisha","F")
@@ -11,4 +15,4 @@ print(h1.count)
 print(h2.count)
 print(h1.name)
 print(h2.name)
-h1.height
+h1.height = 456
