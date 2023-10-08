7ec704bdb9534f7b9db63438ffc018f26890f1db
VarunSharma3250 <sv279508@gmail.com>
2023-10-08 18:36:02 +0530

    reverseLL

--- DSA Python/reverseLL.py
+++ DSA Python/reverseLL.py
@@ -1,17 +1,17 @@
 class Node():
-    def __inti__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
         self.pre = None
-        
-class Linked List():
 
-    def __inti__(self):
+class LinkedList():
+
+    def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
     
-    def append(self,value):
+    def append(self, value):
         node = Node(value)
         if self.head is None:
             self.head = node
@@ -21,12 +21,34 @@ class Linked List():
             self.tail = node
         self.length += 1
     
+    def reverseLL(self):
+        if self.head == None:
+            return -1
+        else:
+            cursor = self.head
+            prev = None
+            while cursor is not None:
+                next_node = cursor.next
+                cursor.next = prev
+                prev = cursor
+                cursor = next_node
+            self.head = prev
+    
+    def showTime(self):
+        cursor = self.head
+        string = ""
+        while cursor is not None:
+            string += str(cursor.value) + " -> "
+            cursor = cursor.next
+        print("head ->", string, "None")
 
-linky = LinkedList
+linky = LinkedList()
 linky.append(2)
 linky.append(3)
 linky.append(4)
 linky.append(5)
 linky.append(6)
 linky.append(7)
+linky.showTime()
+linky.reverseLL()
 linky.showTime()
--- DSA Python/searching algo.py
+++ DSA Python/searching algo.py
@@ -35,25 +35,7 @@ def Binary(arr,elem):
     
     else:
         return None
-    # it has complexity O(log(n))
-    start = 0
-    end = len(arr) - 1
-    while start <= end:
-        
-        mid = start + (end - start)//2
-        
-        if arr[mid] == elem:
-            return [elem,mid]
-            
-        # positioning
-        if arr[mid] < elem:
-            start = mid + 1
-        else:
-            end = mid - 1
-    
-    else:
-        return None
-            
+                
 def BinaryFast(arr,elem):
     # it has complexity O(log(n))
     start = 0
