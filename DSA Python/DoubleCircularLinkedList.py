03fd404dbe86a62bef8ff7e15aa10d88e9180f1d
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-10-13 00:08:03 +0530

    DCLL

---
+++ DSA Python/DoubleCircularLinkedList.py
@@ -0,0 +1,16 @@
+class Node():
+    def __init__(self,data = None):
+        self.data = data
+        self.next = None
+        self.pre = None
+        
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+        
+        .
+
+dcll = LinkedList()
