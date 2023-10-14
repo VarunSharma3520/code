e509c4edf887e0d597ddf5ae4b011492ba6d8163
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-10-14 22:33:16 +0530

    dcll

--- DSA Python/DoubleCircularLinkedList.py
+++ DSA Python/DoubleCircularLinkedList.py
@@ -10,3 +10,35 @@ class LinkedList():
         self.tail = None
         self.length = 0
         
+    def prepend(self,data):
+        pass
+    
+    def insert(self,data,index):
+        pass
+
+    def append(self,data):
+        pass
+
+    def find(self,data):
+        pass
+
+    def get(self,index):
+        pass
+
+    def setat(self,index):
+        pass
+
+    def showTime():
+        pass
+    
+    def popFirst(self):
+        pass
+
+    def pop(self,index):
+        pass
+
+    def remove(self,data):
+        pass
+
+    def drop(self):
+        
