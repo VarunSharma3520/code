ab78d02c598ccb4313edfb3544d4393750446d6f
VarunSharma3520 <sv279508@gmail.com>
2023-10-16 14:23:30 +0000

    setat get

--- DSA Python/DoubleCircularLinkedList.py
+++ DSA Python/DoubleCircularLinkedList.py
@@ -1,17 +1,18 @@
 class Node():
-    def __init__(self,data = None):
+    def __init__(self, data = None):
         self.data = data
         self.next = None
         self.pre = None
-        
+
+
 class LinkedList():
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
-        
-    def prepend(self,data):
-        node= Node(data)
+
+    def prepend(self, data):
+        node = Node(data)
         if self.head == None:
             node.pre = node
             node.next = node
@@ -25,20 +26,31 @@ class LinkedList():
             self.tail.next = node
             self.head = self.head.pre
             self.length += 1
-    
-    def insert(self,data,index):
-        node= Node(data)
+
+    def insert(self, data, index): #not working properly
+        node = Node(data)
         if self.head == None:
             node.pre = node
             node.next = node
             self.head = node
             self.tail = node
             self.length = 1
+        elif self.length < index:
+            self.append(data)
         else:
-            pass
+            cursor = self.head
+            for i in range(self.length - 1):
+                if i == index:
+                    node.pre = cursor
+                    node.next = cursor.next
+                    cursor.next.pre = node
+                    cursor.next = node
+                    self.length += 1
+                    break
+                cursor = cursor.next
 
-    def append(self,data):
-        node= Node(data)
+    def append(self, data):
+        node = Node(data)
         if self.head == None:
             node.pre = node
             node.next = node
@@ -53,14 +65,29 @@ class LinkedList():
             self.tail = self.tail.next
             self.length += 1
 
-    def find(self,data):
-        pass
+    def find(self, data):
+        cursor = self.head
+        for i in range(self.length):
+            if cursor.data == data:
+                print(data,'found at:',i)
+                break
+            cursor = cursor.next
 
-    def get(self,index):
-        pass
+    def get(self, index):
+        cursor = self.head
+        for i in range(self.length):
+            if i == index:
+                print(cursor.data)
+                break
+            cursor = cursor.next
 
-    def setat(self,index):
-        pass
+    def setat(self, data, index):
+        cursor = self.head
+        for i in range(self.length):
+            if i == index:
+                cursor.data = data
+                break
+            cursor = cursor.next
 
     def showTime(self):
         string = 'Head -> '
@@ -70,17 +97,24 @@ class LinkedList():
             cursor = cursor.next
         string += 'Tail'
         print(string)
+        print('<self.length:', self.length, '>')
+
     def popFirst(self):
+        if self.length == 0:
+            return None
+        else:
+            return self.head.data
+            pass
+
+    def pop(self, index):
         pass
 
-    def pop(self,index):
-        pass
-
-    def remove(self,data):
+    def remove(self, data):
         pass
 
     def drop(self):
         pass
+
 
 ll = LinkedList()
 # ll.prepend(2)
@@ -90,13 +124,14 @@ ll.append(2)
 ll.append(3)
 ll.append(4)
 ll.append(6)
-# ll.insert(5)
+# ll.insert(999,0) # Not Working
 ll.prepend(1)
 ll.showTime()
-# ll.find(2)
-# ll.get(2)-
+# ll.find(6)
+# ll.get(2)
 # ll.setat("Varun Sharma",2)
-# ll.popFirst()
+ll.popFirst()
 # ll.pop(1)
 # ll.remove(5)
+ll.showTime()
 ll.drop()
