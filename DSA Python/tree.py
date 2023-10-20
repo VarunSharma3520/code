0843fe4df60f9412379a03a672ed96d1ecfe25a0
VarunSharma3520 <sv279508@gmail.com>
2023-10-20 22:13:04 +0000

    trees python

--- DSA Python/tree.py
+++ DSA Python/tree.py
@@ -1,16 +1,29 @@
+'''
+NOTES:
+
+Depth First Search
+    1. Preorder Traversal = rootNode -> leftNode -> rightNode
+    2. Inorder Traversal = leftNode -> rootNode -> rightNode
+    3. PostOrder Traversal = leftNode -> rightNode -> rootNode 
+Breath First Search
+    1. Level First Traversal = Level wise from left to right
+
+Searching in BinaryTree:
+We use Level order Traversal in Searching because DFS uses queue instead of stack
+    '''
 class TreeNode():
     def __init__(self,data = None):
         self.data = data
         self.leftChild = None
         self.rightChild = None
-        
-newBT = TreeNode("Drinks")
-leftChild = TreeNode("Hot")
-tea = TreeNode("Tea")
-coffee = TreeNode("Coffee")
+
+newBT = TreeNode("1")
+leftChild = TreeNode("2")
+tea = TreeNode("4")
+coffee = TreeNode("5")
 leftChild.leftChild = tea
 leftChild.rightChild = coffee
-rightChild = TreeNode("Cold")
+rightChild = TreeNode("3")
 newBT.leftChild = leftChild
 newBT.rightChild = rightChild
 
@@ -20,21 +33,59 @@ def preOrderTraversal(rootNode):
     print(rootNode.data)
     preOrderTraversal(rootNode.leftChild)
     preOrderTraversal(rootNode.rightChild)
+
 def inOrderTraversal(rootNode):
     if not rootNode:
         return
     inOrderTraversal(rootNode.leftChild)
-    print(rootNode.data)
+    if rootNode.data is not None:print(rootNode.data)
     inOrderTraversal(rootNode.rightChild)
+
 def postOrderTraversal(rootNode):
     if not rootNode:
         return
     postOrderTraversal(rootNode.leftChild)
     postOrderTraversal(rootNode.rightChild)
     print(rootNode.data)
+
 def deleteBT(rootNode):
     rootNode.data = None
     rootNode.leftChild = None
     rootNode.rightChild = None
     return "The BT has been successfully deleted" 
-print(deleteBT(newBT))
+
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        queue = []
+        queue.append(rootNode)
+        while len(queue) !=  0:
+            rootNode = queue.pop(0)
+            print(rootNode.data)
+            if rootNode.leftChild is not None:
+                queue.append(rootNode.leftChild)
+            if rootNode.rightChild is not None:
+                queue.append(rootNode.rightChild)
+
+def find(rootNode, data):
+    if not rootNode:
+        return -1
+    else:
+        queue = []
+        queue.append(rootNode)
+        while len(queue) != 0:
+            rootNode = queue.pop(0)
+            if rootNode.data == data:
+                return 1
+            if rootNode.leftChild is not None:
+                queue.append(rootNode.leftChild)
+            if rootNode.rightChild is not None:
+                queue.append(rootNode.rightChild)
+        return 0
+
+
+# print(deleteBT(newBT))
+# print(preOrderTraversal(newBT))
+print(levelOrderTraversal(newBT))
+print(find(newBT,1))
