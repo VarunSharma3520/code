3d0e0e7fcb9ac487cabf5abbb3884a210ed6c599
VarunSharma3520 <sv279508@gmail.com>
2023-10-26 11:45:15 +0000

    matplotlib.pyplot error

--- Data Science Python/matplotlib.py
+++ Data Science Python/matplotlib.py
@@ -3,18 +3,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-# matplotlib.use('TkAgg')
 
 xpoints = np.array([2,9,8,5,4,3,4])
 ypoints = np.array([0,2,5,9,6,3,4])
 
 plt.plot(xpoints, ypoints)
-# plt.pie(xpoints)
 
+# plt.show()
 plt.savefig('plotlib.png',dpi=300)
 
 
-#Two  lines to make our compiler able to draw:
-# plt.savefig(sys.stdout.buffer)
-# sys.stdout.flush()
 
