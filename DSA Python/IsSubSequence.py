aaca4c4188b2c6e49282357ad317f9c13f9d9d45
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-11-16 09:47:47 -0800

    c

--- DSA Python/IsSubSequence.py
+++ DSA Python/IsSubSequence.py
@@ -7,12 +7,10 @@
 '''
 
 def isSubSequence(str1, str2):
-
     prev = -1
     count = 0
     n = len(str1)
     m = len(str2)
-
     # To use iterate all character of 'str2'.
     for i in range(n):  
         for j in range(m):
@@ -20,7 +18,6 @@ def isSubSequence(str1, str2):
                 prev = j
                 count += 1
                 break
-
     # If all characters of 'str1' are present in 'str2' in same order.
     if(count == n):       
         return True
