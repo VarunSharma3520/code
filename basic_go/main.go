ee51b13508b3cfbcf862177edeffd2075627891f
VarunSharma3250 <sv279508@gmail.com>
2024-07-05 05:04:40 +0530

    learning go

--- basic_go/main.go
+++ basic_go/main.go
@@ -1,19 +1,139 @@
 // First Go program
-package main;
-import "fmt";
+package main
+
+import (
+	// "bufio"
+	"fmt"
+	// "time"
+	// "os"
+	// "strconv"
+	// "time"
+)
+
+// type Person struct {
+// 	name   string
+// 	age    int
+// 	job    string
+// 	salary int
+// }
+
+// func familyName(fname string) {
+// 	fmt.Println("Hello", fname, "Refsnes")
+// 	// return 0
+// }
 
 // Main function
 func main() {
-	/* This is 
-    multi-line comment*/
-
+	/* This is
+	multi-line comment*/
+  fmt.Println("Hello world")
 	// var variablename type = value
-	// var student1 string = "John" //type is string 
+	// var student1 string = "John" //type is string
 	// var student2 = "Jane" //type is inferred
 	//    x := 2 //type is inferred
 	// var student1 string // variable init
 	//    student1 = "John" // variable assign
 	// var a, b, c, d int = 1, 3, 5, 7
+	// c, d := 7, "World!"
+	// Camel Case -> myVariableName = "John"
+	// Pascal Case -> MyVariableName = "John"
+	// Snake Case -> my_variable_name = "John"
+	// Declaring a Constant
+	// Constant names are usually written in uppercase letters (for easy identification and differentiation from variables)
+	// var arr1 = [3]int{1,2,3}
+	// arr2 := [5]int{4,5,6,7,8}
+	// var arr1 = [...]int{1,2,3}
+	// arr2 := [...]int{4,5,6,7,8}
+	// myslice := []int{1,2,3}
+	// prices := []int{10,20,30}
+	// fmt.Println(prices[0])
+	// fmt.Println(prices[2])
+	// time := 22
+	// if time < 10 {
+	//   fmt.Println("Good morning.")
+	// } else if time < 20 {
+	//   fmt.Println("Good day.")
+	// } else {
+	//   fmt.Println("Good evening.")
+	// }
+	// // switch statement
+	// day := 4
+	//
+	// switch day {
+	// case 1:
+	//   fmt.Println("Monday")
+	// case 2:
+	//   fmt.Println("Tuesday")
+	// case 3:
+	//   fmt.Println("Wednesday")
+	// case 4:
+	//   fmt.Println("Thursday")
+	// case 5:
+	//   fmt.Println("Friday")
+	// case 6:
+	//   fmt.Println("Saturday")
+	// case 7:
+	//   fmt.Println("Sunday")
+	// }
+	// for i:=0; i < 5; i++ {
+	// fmt.Println(i)
+	// }
+	// fruits := [3]string{"apple", "orange", "banana"}
+	// for idx, val := range fruits {
+	//    fmt.Printf("%v\t%v\n", idx, val)
+	// }
+	// var a = map[KeyType]ValueType{key1:value1, key2:value2,...}
+	// b := map[KeyType]ValueType{key1:value1, key2:value2,...}
+	// ar a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+	// b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+	// var p1 Person
+	// var p2 Person
+	// familyName("giva")
+	// const PI = 3.14
+	// fmt.Printf("i has value: %v and type: %T\n", PI, PI)
+	// Pers1 specification
+	// p1.name = "Hege"
+	// p1.age = 45
+	// p1.job = "Teacher"
+	// p1.salary = 6000
 
-	fmt.Println("Hello World")
+	// // p2 specification
+	// p2.name = "Cecilie"
+	// p2.age = 24
+	// p2.job = "Marketing"
+	// p2.salary = 4500
+
+	// // Access and print p1 info
+	// fmt.Print("Name: ", p1.name)
+	// fmt.Print("Age: ", p1.age)
+	// fmt.Print("Job: ", p1.job)
+	// fmt.Print("Salary: ", p1.salary)
+
+	// // Access and print p2 info
+	// fmt.Print("Name: ", p2.name)
+	// fmt.Print("Age: ", p2.age)
+	// fmt.Print("Job: ", p2.job)
+	// fmt.Print("Salary: ", p2.salary)
+  // fmt.Println("Enter a value")
+	// reader := bufio.NewReader(os.Stdin)
+	// input, _ := reader.ReadString('\n')
+	// fmt.Println("You entered:", input)
+  // fmt.Printf("%T", input)
+  // num, err := strconv.ParseFloat(input, 64)
+  // fmt.Printf("%T", num)
+  // fmt.Println(num)
+  // if err != nil {
+  //   fmt.Println("Error: ", err)
+  // }
+  // presentTime := time.Now()
+  // fmt.Println("The time is", presentTime.Format("01-02-2006 15:04:05 Monday"))
+  // createddate := time.Date(2020,time.August,10,23,23,0,0, time.UTC)
+  // fmt.Println("The created date is", createddate.Format("01-02-2006 15:04:05 Monday"))
+  // myNumber := 23
+  // var ptr = &myNumber // address of 
+  // fmt.Println("value of ptr is:",ptr)
+  // fmt.Println("value of ptr is:",*ptr)  // value of
+
+
+
 }
