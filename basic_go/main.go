dd30d2e001dd26939d23362fb7de80c7c414c964
VarunSharma3250 <sv279508@gmail.com>
2024-07-04 17:01:24 +0530

    c

--- basic_go/main.go
+++ basic_go/main.go
@@ -8,12 +8,12 @@ func main() {
     multi-line comment*/
 
 	// var variablename type = value
-	var student1 string = "John" //type is string 
-	var student2 = "Jane" //type is inferred
-    x := 2 //type is inferred
-	var student1 string // variable init
-    student1 = "John" // variable assign
-	var a, b, c, d int = 1, 3, 5, 7
+	// var student1 string = "John" //type is string 
+	// var student2 = "Jane" //type is inferred
+	//    x := 2 //type is inferred
+	// var student1 string // variable init
+	//    student1 = "John" // variable assign
+	// var a, b, c, d int = 1, 3, 5, 7
 
 	fmt.Println("Hello World")
 }
