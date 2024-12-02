d583666a3705355dd0cf84a8a2807694cadedc09
VarunSharma3520 <sv279508@gmail.com>
2024-12-03 02:41:32 +0530

    learn_zustand 2

--- learn_zustand/src/App.jsx
+++ learn_zustand/src/App.jsx
@@ -4,12 +4,8 @@ function App() {
   // const count = useStore((state) => state.count);
   // const inc = useStore((state) => state.inc);
   // const dec = useStore((state) => state.dec);
-
-  const { count, inc, dec } = useStore((state) => ({
-    count: state.count,
-    inc: state.inc,
-    dec: state.dec,
-  }));
+  //                 or
+  const { count, inc, dec } = useStore((state) => (state));
 
   const name = useNameStore((state) => state.name);
   const setName = useNameStore((state) => state.setName);
@@ -17,34 +13,32 @@ function App() {
   const girl = useNameStore((state) => state.girl);
 
   return (
-    <>
       <div className="card">
         <input
           type="Name"
           placeholder={name}
-          onChange={(e) => setName(e.target.value)}
+        onChange={(e) => {
+            e.preventDefault();
+            setName(e.target.value);
+          }}
         />
 
-        {[name, count].map((value, index) => {
-          console.log(`value: ${value}, index: ${index}`)
-          return <p key={index}>{value}</p>;
-        })}
+        <p>Name: {name}</p>
+        <p>Count: {count}</p>
 
         {[
           { label: "Increment", onClick: inc },
           { label: "Decrement", onClick: dec },
           { label: 'Add "boy"', onClick: boy },
           { label: 'Add "girl"', onClick: girl },
-        ].map((action, index) => (
+        ].map((value, index) => (
           <>
-            <button key={index} onClick={action.onClick}>
-              {action.label}
+            <button key={index} onClick={value.onClick}>
+              {value.label}
             </button>
-            <br />
           </>
         ))}
       </div>
-    </>
   );
 }
 
